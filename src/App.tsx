9f322da8e0663b78b7125d65ea6321a489db9e1c
jhosep98 <jhosep149@hotmail.com>
2022-01-27 13:24:15 -0300

    fix init config

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Init</h1>
-      </header>
+      <h1>Init Project</h1>
     </div>
   );
 }
